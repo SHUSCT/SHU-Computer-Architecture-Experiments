66fd4e01dbd4ede4e436c456a6236546728104d3
jamesnulliu <jamesnulliu@outlook.com>
2024-04-01 01:10:34 +0800

    OneAPI vs STL

--- exp02-openmp-src/computePi_main.cpp
+++ exp02-openmp-src/computePi_main.cpp
@@ -1,7 +1,7 @@
-#include "xRandom.hpp"
 #include "TimeCounter.hpp"
-#include <iostream>
+#include "xRandom.hpp"
 #include <format>
+#include <iostream>
 #include <omp.h>
 
 double computePi(int n)
@@ -20,13 +20,33 @@ double computePi_OMP(int n, int num_threads)
     double pi = 0.0;
     double step = 1.0 / n;
     omp_set_num_threads(num_threads);
-#pragma omp parallel for reduction(+:pi)
+#pragma omp parallel for reduction(+ : pi)
     for (int i = 0; i < n; ++i) {
         double x = (i + 0.5) * step;
         pi += 4.0 / (1.0 + x * x);
     }
     return pi * step;
 }
+
+double computePi_STL(int n, int num_threads)
+{
+    double pi = 0.0;
+    double step = 1.0 / n;
+    std::vector<int> steps(n);
+    // std::iota(steps.begin(), steps.end(), 0);
+
+    /**
+     * @bug One API seems to conflict with some STL algorithms?
+     */
+    // pi = std::transform_reduce(std::execution::par_unseq, steps.begin(), steps.end(), 0.0,
+    //                            std::plus<double>(), [step](int i) {
+    //                                double x = (i + 0.5) * step;
+    //                                return 4.0 / (1.0 + x * x);
+    //                            });
+    return pi * step;
+}
+
+
 
 int main()
 {
@@ -67,7 +87,16 @@ int main()
     timeCounter.startCounting();
     pi_OMP = computePi_OMP(n, 16);
     timeCounter.endCounting();
-    std::cout << std::format("OMP with 16 threads: {} ms, pi = {}\n", timeCounter.msecond(), pi_OMP);
+    std::cout << std::format("OMP with 16 threads: {} ms, pi = {}\n", timeCounter.msecond(),
+                             pi_OMP);
+
+    // // OMP STL
+    // timeCounter.init();
+    // timeCounter.startCounting();
+    // pi_OMP = computePi_STL(n, 16);
+    // timeCounter.endCounting();
+    // std::cout << std::format("OMP with 16 threads: {} ms, pi = {}\n", timeCounter.msecond(),
+    //                          pi_OMP);
 
     double accleration = double(baseline) / timeCounter.msecond();
     std::cout << std::format("Accleration ratio: {}", accleration);
