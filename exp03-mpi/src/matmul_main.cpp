656e56c260d678400f529a63f66c848ae7aea41f
jamesnulliu <jamesnulliu@outlook.com>
2024-04-24 09:51:43 +0800

    Update Yutils and Matmul :

--- exp03-mpi/src/matmul_main.cpp
+++ exp03-mpi/src/matmul_main.cpp
@@ -8,13 +8,40 @@
 #include <Yutils/Logger.hpp>
 #include <Yutils/Random.hpp>
 #include <Yutils/SimpleWriter.hpp>
+#include <Yutils/TimeCounter.hpp>
 
-void matMult(double* a, double* b, double* c, int nCol, int nRow) {
-    for (int i = 0; i < nCol; i++) {
-        for (int j = 0; j < nRow; j++) {
-            c[i * nRow + j] = 0;
-            for (int k = 0; k < nRow; k++) {
-                c[i * nRow + j] += a[i * nRow + k] * b[k * nRow + j];
+/**
+ * @brief Multiply two matrices A and B, and store the result in matrix C.
+ * 
+ * @param A Matrix A, with shape (n, m)
+ * @param B Matrix B, with shape (m, m)
+ * @param C Matrix C, with shape (n, m)
+*/
+void matMult(double* A, double* B, double* C, int n, int m) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            C[i * m + j] = 0;
+            for (int k = 0; k < m; k++) {
+                C[i * m + j] += A[i * m + k] * B[k * m + j];
+            }
+        }
+    }
+}
+
+/**
+ * @brief Multiply two matrices A and B, and store the result in matrix C.
+ * 
+ * @param A Matrix A, with shape (n, m)
+ * @param B Matrix B, with shape (m, m)
+ * @param C Matrix C, with shape (n, m)
+*/
+void matMult_OMP(double* A, double* B, double* C, int n, int m) {
+    #pragma omp parallel for
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            C[i * m + j] = 0;
+            for (int k = 0; k < m; k++) {
+                C[i * m + j] += A[i * m + k] * B[k * m + j];
             }
         }
     }
@@ -39,10 +66,11 @@ int main(int argc, char** argv)
     yutils::ArgParser parser;
     parser.addOption("n", "Matrix Size", "int", "4");
     parser.addOption("o", "Output File Path", "string", "output.csv");
-
     parser.parse(argc, argv);
-    int matSize = *parser.get<int>("n");
-    std::string outPath = *parser.get<std::string>("o");
+    auto matSize = parser.get<int>("n").value();
+    auto outPath = parser.get<std::string>("o").value();
+
+    yutils::TimeCounter timeCounter;
 
     std::vector<double> a(matSize * matSize, 0), b(matSize * matSize, 0), c(matSize * matSize, 0),
         sub_a(matSize * matSize / nRanks), sub_c(matSize * matSize / nRanks);
@@ -50,6 +78,8 @@ int main(int argc, char** argv)
     if (rank == 0) {
         initMat(a);
         initMat(b);
+        timeCounter.init();
+        timeCounter.startCounting();
     }
 
     MPI_Scatter(a.data(), matSize * matSize / nRanks, MPI_DOUBLE, sub_a.data(),
@@ -60,17 +90,19 @@ int main(int argc, char** argv)
                matSize * matSize / nRanks, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
     if (rank == 0) {
-        std::string row;
-        for (int i = 0; i < matSize; i++) {
-            row.clear();
-            for (int j = 0; j < matSize; j++) {
-                row += std::to_string(c[i * matSize + j]);
-                if (j != matSize - 1) {
-                    row += ",";
-                }
-            }
-            std::cout << row << std::endl;
-        }
+        timeCounter.endCounting();
+        // std::string row;
+        // for (int i = 0; i < matSize; i++) {
+        //     row.clear();
+        //     for (int j = 0; j < matSize; j++) {
+        //         row += std::to_string(c[i * matSize + j]);
+        //         if (j != matSize - 1) {
+        //             row += ",";
+        //         }
+        //     }
+        //     std::cout << row << std::endl;
+        // }
+        YCRITICAL("Time taken: {}ms", timeCounter.msecond());
     }
 
     MPI_Finalize();
